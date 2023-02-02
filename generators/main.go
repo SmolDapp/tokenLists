6c81f32352b5e4744cd513aec60e82f210693e17
Majorfi <major-tom.eth@pm.me>
2023-02-02 17:15:34 +0100

    feat: more workflow

--- generators/main.go
+++ generators/main.go
@@ -1,30 +1,47 @@
 package main
 
 import (
+	"os"
+	"strings"
+
 	"github.com/migratooor/tokenLists/generators/common/ethereum"
 	"github.com/migratooor/tokenLists/generators/common/helpers"
 	"github.com/migratooor/tokenLists/generators/common/logs"
 )
 
+var instructionToFunction = map[string]func(){
+	`coingecko`:       buildCoingeckoTokenList,
+	`1inch`:           build1InchTokenList,
+	`paraswap`:        buildParaswapTokenList,
+	`defillama`:       buildDefillamaTokenList,
+	`yearn`:           buildYearnTokenList,
+	`curve`:           buildCurveTokenList,
+	`cowswap`:         buildCowswapTokenList,
+	`uniswap`:         buildUniswapTokenList,
+	`sushiswap`:       buildSushiswapTokenList,
+	`ledgers`:         buildLedgersTokenList,
+	`portals`:         buildPortalsTokenList,
+	`wido`:            buildWidoTokenList,
+	`uniswap-pairs`:   buildUniswapPairsTokenList,
+	`sushiswap-pairs`: buildSushiswapPairsTokenList,
+}
+
 func main() {
 	helpers.Init()
 	ethereum.Init()
-	logs.Info(`Welcome to the tokenLists generator!`)
-	// buildLedgersTokenList()
-	// buildPortalsTokenList()
-	// buildWidoTokenList()
-	buildCoingeckoTokenList()
-	// build1InchTokenList()
-	// buildParaswapTokenList()
-	// buildDefillamaTokenList()
-	// buildYearnTokenList()
-	// buildCurveTokenList()
-	// buildCowswapTokenList()
 
-	// buildUniswapTokenList()
-	// buildUniswapPairsTokenList()
-
-	// buildSushiswapTokenList()
-	// buildSushiswapPairsTokenList()
-	logs.Info(`Done!`)
+	if len(os.Args) < 2 {
+		for name, generator := range instructionToFunction {
+			logs.Info(`Running generator:`, strings.ToTitle(name))
+			generator()
+			logs.Success(`Done!`)
+		}
+	} else {
+		//grab each arg and run the generator
+		for _, arg := range os.Args[1:] {
+			logs.Info(`Running generator:`, strings.ToTitle(arg))
+			instructionToFunction[arg]()
+			logs.Success(`Done!`)
+		}
+	}
 }
