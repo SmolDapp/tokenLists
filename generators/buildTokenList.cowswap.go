3bdadeba4f44df47a10d5ee2b04414b2e066bc25
Majorfi <major-tom.eth@pm.me>
2023-05-01 13:18:49 +0200

    fix: upd cow logo

--- generators/buildTokenList.cowswap.go
+++ generators/buildTokenList.cowswap.go
@@ -8,7 +8,7 @@ func buildCowswapTokenList() {
 	tokenList := loadTokenListFromJsonFile(`cowswap.json`)
 	originalTokenList := helpers.FetchJSON[TokenListData](`https://raw.githubusercontent.com/cowprotocol/token-lists/main/src/public/CowSwap.json`)
 	tokenList.Name = originalTokenList.Name
-	tokenList.LogoURI = originalTokenList.LogoURI
+	tokenList.LogoURI = `https://raw.githubusercontent.com/cowprotocol/cowswap/c5974fb8a45d678029ecb013dab33722e152daaa/src/assets/cow-swap/cow_v2.svg`
 	tokenList.Keywords = originalTokenList.Keywords
 	tokenList = tokenList.Assign(originalTokenList.Tokens)
 
