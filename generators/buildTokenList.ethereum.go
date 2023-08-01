48528a654dbe97e30de5a18399422419f20a03a3
Majorfi <major-tom.eth@pm.me>
2023-08-01 11:01:07 +0200

    fix: replace etherscan logo

--- generators/buildTokenList.ethereum.go
+++ generators/buildTokenList.ethereum.go
@@ -63,7 +63,7 @@ func fetchEthereumTokenList(currentPage uint8) []TokenListToken {
 func buildEthereumTokenList() {
 	tokenList := loadTokenListFromJsonFile(`ethereum-etherscan.json`)
 	tokenList.Name = `Ethereum via Etherscan`
-	tokenList.LogoURI = `https://raw.githubusercontent.com/maticnetwork/zkevm-docs/main/static/img/polygon-zkevm-logo.svg`
+	tokenList.LogoURI = `https://2163491710-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2F-McrExXKKJBLJqymbFhO%2Fuploads%2Fgit-blob-29966808954100660b18274b582c1afb2fe45fed%2Fetherscan-logo-circle.png?alt=media`
 	tokenList.Keywords = []string{`ethereum`, `etherscan`}
 	tokens := fetchEthereumTokenList(1)
 	saveTokenListInJsonFile(tokenList, tokens, `ethereum-etherscan.json`, Standard)
