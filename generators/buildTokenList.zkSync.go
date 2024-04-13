1a0b13fc9d81d5d3121c15108b50fb8c692975b2
Major <major@ycorpo.com>
2024-04-13 17:17:37 +0200

    feat: migrating to dynamic zksync list

--- generators/buildTokenList.zkSync.go
+++ generators/buildTokenList.zkSync.go
@@ -5,29 +5,52 @@ import (
 	"github.com/migratooor/tokenLists/generators/common/chains"
 	"github.com/migratooor/tokenLists/generators/common/helpers"
 	"github.com/migratooor/tokenLists/generators/common/models"
-	"github.com/migratooor/tokenLists/generators/static"
 )
 
-func handleZkSyncTokenList(chainID uint64, tokens []static.TStaticElement) []models.TokenListToken {
-	tokenAddresses := []common.Address{}
-	for _, token := range tokens {
-		tokenAddresses = append(tokenAddresses, token.Address)
-	}
+func handleZkSyncTokenList(chainID uint64, tokenAddresses []common.Address) []models.TokenListToken {
 	tokenList := helpers.GetTokensFromAddresses(chainID, tokenAddresses)
 	tokenList = append(tokenList, chains.CHAINS[chainID].Coin)
 	return tokenList
 }
 
 func fetchZkSyncTokenList() []models.TokenListToken {
-	tokens := static.ZKSYNC_STATIC_TOKENLIST
-	return handleZkSyncTokenList(324, tokens[324])
+	type TZkSyncAPIResponse struct {
+		Items []struct {
+			Address string `json:"l2address"`
+			IconURI string `json:"iconURL"`
+		} `json:"items"`
+		Meta struct {
+			TotalItems int `json:"totalItems"`
+		}
+		Links struct {
+			Next string `json:"next"`
+		} `json:"links"`
+	}
+
+	baseAPIEndpoint := `https://block-explorer-api.mainnet.zksync.io/`
+	nextPageURI := `tokens?page=1&limit=100&minLiquidity=0`
+	tokens := []common.Address{}
+	for i := 0; i < 40; i++ {
+		response := helpers.FetchJSON[TZkSyncAPIResponse](baseAPIEndpoint + nextPageURI)
+		for _, token := range response.Items {
+			tokens = append(tokens, common.HexToAddress(token.Address))
+		}
+		nextPageURI = response.Links.Next
+		if nextPageURI == `` {
+			break
+		}
+		if len(tokens) >= response.Meta.TotalItems {
+			break
+		}
+	}
+	return handleZkSyncTokenList(324, tokens)
 }
 
 func buildZkSyncTokenList() {
 	tokenList := helpers.LoadTokenListFromJsonFile(`zksync.json`)
 	tokenList.Name = `zkSync`
 	tokenList.LogoURI = `https://assets.smold.app/api/chain/324/logo-128.png`
-	tokenList.Keywords = []string{`zksync`}
+	tokenList.Keywords = []string{`zksync`, `explorer`}
 	tokens := []models.TokenListToken{}
 	tokens = append(tokens, fetchZkSyncTokenList()...)
 	helpers.SaveTokenListInJsonFile(tokenList, tokens, `zksync.json`, helpers.SavingMethodStandard)
